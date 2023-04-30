d19d2b917183966b88ce3fc424b00cf4eb276b4a
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-05-01 03:51:15 +0900

    [Feat] 아웃라인 구현

---
+++ HoloCure/Assets/1_Scripts/Player/OutLineSensorRight.cs
@@ -0,0 +1,22 @@
+﻿using StringLiterals;
+using UnityEngine;
+
+public class OutLineSensorRight : MonoBehaviour
+{
+    private BoxCollider2D _collider;
+    private void Awake()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+        _collider.isTrigger = true;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(TagLiteral.ENEMY_BODY))
+        {
+            Enemy enemy = collision.transform.parent.GetComponent<Enemy>();
+            enemy.transform.position = (Vector2)enemy.transform.position + (Util.Caching.CenterWorldPos - (Vector2)enemy.transform.position) * 1.8f;
+            enemy.SetFilpX();
+        }
+    }
+}
+
