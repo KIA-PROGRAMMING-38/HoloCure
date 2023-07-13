298c05d868a549974d55d5e92fcbac88d7520fb3
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-07-13 20:12:49 +0900

    [Refactor] 타입구분 기능 추가, 경험치와 데미지텍스트 구조 변경

--- HoloCure/Assets/1_Scripts/Object/Exp.cs
+++ HoloCure/Assets/1_Scripts/Object/Exp.cs
@@ -2,40 +2,57 @@ using StringLiterals;
 using System;
 using System.Collections;
 using UnityEngine;
-using Util.Pool;
-public enum ExpAmounts
-{
-    Default_One,
-    Two = 10,
-    Three = 20,
-    Four = 50,
-    Five = 100,
-    Max_Six = 200,
-}
+
 public class Exp : MonoBehaviour
 {
-    public event Func<Vector2, int, Exp> OnTriggerWithExp;
+    public event Func<Vector2, int, Exp> OnTrigger;
     public int ExpAmount { get; private set; }
     public bool IsReleased { get; private set; }
+    private SpriteRenderer _spriteRenderer;
 
+    private void Awake()
+    {
+        GetComponent<CircleCollider2D>().isTrigger = true;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _moveRandomCo = MoveRandomCo();
+    }
+    public void Init(Vector2 position, int expAmount)
+    {
+        transform.position = position;
+        ExpAmount = expAmount;
+        IsReleased = false;
+        _accumulatedSpeed = 100f;
+        _elapsedTime = 0;
+
+        ExpType type = ExpAmount.GetExpType();
+        _spriteRenderer.sprite = GetSprite(type);
+
+        static Sprite GetSprite(ExpType type)
+        {
+            switch (type)
+            {
+                case ExpType.Zero: return Managers.Resource.LoadSprite(Managers.Data.Exp[0].Sprite);
+                case ExpType.One: return Managers.Resource.LoadSprite(Managers.Data.Exp[1].Sprite);
+                case ExpType.Two: return Managers.Resource.LoadSprite(Managers.Data.Exp[2].Sprite);
+                case ExpType.Three: return Managers.Resource.LoadSprite(Managers.Data.Exp[3].Sprite);
+                case ExpType.Four: return Managers.Resource.LoadSprite(Managers.Data.Exp[4].Sprite);
+                case ExpType.Max: return Managers.Resource.LoadSprite(Managers.Data.Exp[5].Sprite);
+                default: throw new ArgumentOutOfRangeException(nameof(type));
+            }
+        }
+    }
     public void ReleaseToPool()
     {
         IsReleased = true;
         Managers.Pool.Exp.Release(this);
     }
-
-    private void Awake()
+    private float _elapsedTime;
+    private readonly static Vector3 s_floatingVec = new Vector3(0, 0.075f, 0);
+    private void Update()
     {
-        GetComponent<CircleCollider2D>().isTrigger = true;
-        
-        _moveRandomCo = MoveRandomCo();
-    }
-    public void Init(Vector2 pos, int expAmount)
-    {
-        transform.position = pos;
-        ExpAmount = expAmount;
-        IsReleased = false;
-        _accumulatedSpeed = 100f;
+        _elapsedTime += Time.deltaTime;
+        transform.position += s_floatingVec * Mathf.Sin(_elapsedTime * 5);
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
@@ -61,70 +78,69 @@ public class Exp : MonoBehaviour
             return;
         }
 
-        if (ExpAmount >= (int)ExpAmounts.Max_Six) { return; }
+        if (ExpAmount.GetExpType() >= ExpType.Max) { return; }
 
         if (collision.CompareTag(TagLiteral.EXP))
         {
             Exp exp = collision.GetComponent<Exp>();
 
-            if (exp.ExpAmount >= (int)ExpAmounts.Max_Six) { return; }
+            if (exp.ExpAmount.GetExpType() >= ExpType.Max) { return; }
 
             exp.ReleaseToPool();
             ReleaseToPool();
 
-            OnTriggerWithExp?.Invoke(transform.position, exp.ExpAmount + this.ExpAmount);
+            OnTrigger?.Invoke(transform.position, exp.ExpAmount + this.ExpAmount);
         }
     }
     private float _accumulatedSpeed;
-    /// <summary>
-    /// 플레이어를 향해 움직이는 코루틴을 실행시킵니다. 오브젝터 센서에 감지되면 호출됩니다.
-    /// </summary>
     private void MoveToPlayer()
     {
         _accumulatedSpeed += _accumulatedSpeed * Time.deltaTime;
 
-        transform.Translate((Util.Caching.CenterWorldPos - (Vector2)transform.position).normalized * _accumulatedSpeed * Time.deltaTime);
+        transform.Translate(_accumulatedSpeed * Time.deltaTime * (Util.Caching.CenterWorldPos - (Vector2)transform.position).normalized);
     }
-    private Vector2 _initPos;
-    private Vector2 _movePos;
+    private Vector2 _startPoint;
+    private Vector2 _endPoint;
     private float _spawnMoveTime;
     /// <summary>
     /// 랜덤한 위치로 움직이는 코루틴을 실행시킵니다. 적 사망시 호출됩니다.
     /// </summary>
-    public void SpawnMove(Vector2 pos)
+    public void SpawnMove(Vector2 position)
     {
-        _initPos = pos;
-        _movePos = SetMovePos();
+        _startPoint = position;
+        _endPoint = GetEndPoint(_startPoint);
         _spawnMoveTime = 0;
 
         StartCoroutine(_moveRandomCo);
-    }
-    private Vector2 SetMovePos()
-    {
-        int x, y;
-        if (UnityEngine.Random.Range(0, 2) == 0)
-        {
-            x = UnityEngine.Random.Range(0, 2) == 0 ? UnityEngine.Random.Range(-30, -19) : UnityEngine.Random.Range(20, 31);
-            y = UnityEngine.Random.Range(-30, 31);
-        }
-        else
-        {
-            x = UnityEngine.Random.Range(-30, 31);
-            y = UnityEngine.Random.Range(0, 2) == 0 ? UnityEngine.Random.Range(-30, -19) : UnityEngine.Random.Range(20, 31);
-        }
 
-        return _initPos + Vector2.right * x + Vector2.up * y;
+        static Vector2 GetEndPoint(Vector2 position)
+        {
+            int x, y;
+            if (UnityEngine.Random.Range(0, 2) == 0)
+            {
+                x = UnityEngine.Random.Range(0, 2) == 0 ? UnityEngine.Random.Range(-30, -19) : UnityEngine.Random.Range(20, 31);
+                y = UnityEngine.Random.Range(-30, 31);
+            }
+            else
+            {
+                x = UnityEngine.Random.Range(-30, 31);
+                y = UnityEngine.Random.Range(0, 2) == 0 ? UnityEngine.Random.Range(-30, -19) : UnityEngine.Random.Range(20, 31);
+            }
+
+            return position + Vector2.right * x + Vector2.up * y;
+        }
     }
+    private const float MOVE_TIME = 0.2f;
     private IEnumerator _moveRandomCo;
     private IEnumerator MoveRandomCo()
     {
         while (true)
         {
-            while (_spawnMoveTime <= 0.2f)
+            while (_spawnMoveTime <= MOVE_TIME)
             {
                 _spawnMoveTime += Time.deltaTime;
 
-                transform.position = Vector2.Lerp(_initPos, _movePos, _spawnMoveTime / 0.2f);
+                transform.position = Vector2.Lerp(_startPoint, _endPoint, _spawnMoveTime / MOVE_TIME);
 
                 yield return null;
             }
