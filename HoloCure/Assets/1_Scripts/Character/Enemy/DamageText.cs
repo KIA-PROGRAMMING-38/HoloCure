00b632467aab7f2c3ae16a0509bdfb5ee54b3dfc
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-07-13 12:10:49 +0900

    [Refactor] ExclamationMark 제거

--- HoloCure/Assets/1_Scripts/Character/Enemy/DamageText.cs
+++ HoloCure/Assets/1_Scripts/Character/Enemy/DamageText.cs
@@ -1,15 +1,10 @@
 using Cysharp.Text;
 using StringLiterals;
-using System;
 using TMPro;
 using UnityEngine;
 
 public class DamageText : MonoBehaviour
 {
-    public event Action<Vector2> OnMove;
-    public event Action<Color> OnFade;
-    public event Action<Color> OnInitialize;
-
     private TextMeshProUGUI _text;
 
     private Vector2 _startPoint;
@@ -34,8 +29,6 @@ public class DamageText : MonoBehaviour
 
         _elapsedTime = 0;
         _text.color = new Color(1, 1, 1, 1);
-
-        OnInitialize?.Invoke(_text.color);
     }
     public void InitDefaultDamage(Vector2 pos, Vector2 dir, int damage)
     {
@@ -55,13 +48,11 @@ public class DamageText : MonoBehaviour
     {
         _elapsedTime += Time.deltaTime;
         transform.position = Util.BezierCurve.Quadratic(_startPoint, _wayPoint, _endPoint, _elapsedTime / FLOATING_TIME);
-        OnMove?.Invoke(transform.position);
+
         if (_elapsedTime >= FADE_START_TIME)
         {
             float fadeRate = 1 - (_elapsedTime - FADE_START_TIME) / FADE_DURATION_TIME;
             _text.color = new Color(1, 1, 1, fadeRate);
-
-            OnFade?.Invoke(_text.color);
         }
 
         if (_elapsedTime >= FLOATING_TIME)
