a4cf000a66c4c78d8b2abb6aaca0b0accc8e0508
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-07-17 08:24:19 +0900

    [Refactor] 기존 타이틀 UI 관련 스크립트 제거, 수정

--- HoloCure/Assets/1_Scripts/UI/TitleBarMovement.cs
+++ HoloCure/Assets/1_Scripts/UI/TitleBarMovement.cs
@@ -1,17 +1,25 @@
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 public class TitleBarMovement : MonoBehaviour
 {
     private RectTransform _rectTransform;
-    private void Awake() => _rectTransform = GetComponent<RectTransform>();
     private readonly Vector2 MOVE_SPEED = new(-0.5f, 0);
     private readonly Vector2 MOVE_POINT = new(-40, 0);
-    private void Update()
+    private void Awake() => _rectTransform = gameObject.GetComponentAssert<RectTransform>();
+    private void Start()
     {
-        _rectTransform.anchoredPosition += MOVE_SPEED;
-        if (_rectTransform.anchoredPosition == MOVE_POINT)
+        this.UpdateAsObservable()
+            .Subscribe(Move);
+
+        void Move(Unit unit)
         {
-            _rectTransform.anchoredPosition = default;
+            _rectTransform.anchoredPosition += MOVE_SPEED;
+            if (_rectTransform.anchoredPosition == MOVE_POINT)
+            {
+                _rectTransform.anchoredPosition = default;
+            }
         }
     }
 }
