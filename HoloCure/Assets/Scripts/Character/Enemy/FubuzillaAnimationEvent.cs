ffffa29226f069b37d1981c34ea77a6a546f4a71
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-07-28 11:28:36 +0900

    [Chore] 클래스명 변경

--- HoloCure/Assets/Scripts/Character/Enemy/FubuzillaAnimationEvent.cs
+++ HoloCure/Assets/Scripts/Character/Enemy/FubuzillaAnimationEvent.cs
@@ -1,15 +1,10 @@
 using UnityEngine;
 
-public class FubuzillaAttackIndicator : MonoBehaviour
+public class FubuzillaAnimationEvent : MonoBehaviour
 {
     private GameObject _warningLine;
     private void Awake() => _warningLine = transform.GetChild(0).gameObject;
-    /// <summary>
-    /// 애니메이션 이벤트에서 호출됩니다.
-    /// </summary>
+    
     public void ShowWarningLine() => _warningLine.SetActive(true);
-    /// <summary>
-    /// 애니메이션 이벤트에서 호출됩니다.
-    /// </summary>
     public void HideWarningLine() => _warningLine.SetActive(false);
 }
