b3fe009efd6c4288544ae592cd3fed0314fe6c52
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-08-04 17:07:29 +0900

    [Feat] 전용 무기 BrightStar 추가

---
+++ HoloCure/Assets/Scripts/Weapon/BrightStar.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class BrightStar : CursorTargetingRangedWeapon
+{
+    private const float ANGLE_BETWEEN_STRIKES = 40f;
+    private float _strikeMoveTime;
+    private void Awake()
+    {
+        angleBetweenStrikes = ANGLE_BETWEEN_STRIKES;
+        _strikeMoveTime = Managers.Data.WeaponLevelTable[ItemID.BirdFeather][1].AttackDurationTime;
+    }
+
+    protected override void SetupStrikeOnPerform(WeaponStrike strike, int strikeIndex)
+    {
+        strike.transform.rotation = centerStrikeRotation * Quaternion.AngleAxis(angles[strikeIndex], Vector3.back);
+
+        Managers.Sound.Play(SoundID.BirdFeather);
+    }
+
+    protected override void StrikeOperate(WeaponStrike strike)
+    {
+        if (strike.OperateTime > _strikeMoveTime) { return; }
+
+        base.StrikeOperate(strike);
+    }
+}
