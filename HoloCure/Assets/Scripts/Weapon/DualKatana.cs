299665e85bdf2fa698f12d52b622ecf79425c266
Yoo-JaeKwang <jaeu4100@gmail.com>
2023-08-07 03:20:41 +0900

    [Feat] 전용 무기 DualKatana 추가

---
+++ HoloCure/Assets/Scripts/Weapon/DualKatana.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Util;
+
+public class DualKatana : CursorTargetingMeleeWeapon
+{
+    private const float ANGLE_BETWEEN_STRIKES = 30f;
+    private float[] _angles = new float[Define.MAX_STRIKE_COUNT];
+    private Quaternion _centerStrikeRotation;
+
+    public override void LevelUp()
+    {
+        base.LevelUp();
+        Utils.SetAnglesFromCenter(_angles, weaponData.StrikeCount, ANGLE_BETWEEN_STRIKES);
+    }
+
+    protected override void SetupStrikeOnPerform(WeaponStrike strike, int strikeIndex)
+    {
+        if (strikeIndex == 0)
+        {
+            _centerStrikeRotation = strike.transform.rotation;
+        }
+        strike.transform.rotation = _centerStrikeRotation * Quaternion.AngleAxis(_angles[strikeIndex], Vector3.back);
+
+        Managers.Sound.Play(SoundID.DualKatana);
+    }
+}
