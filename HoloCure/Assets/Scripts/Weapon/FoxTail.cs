0f814f5a00c66d4b50dc57a1897c016fddf42907
Yoo-JaeKwang <120005225+Yoo-JaeKwang@users.noreply.github.com>
2023-08-07 14:38:09 +0900

    [Feature] #197 전용 무기 FoxTail 추가 (#199)

---
+++ HoloCure/Assets/Scripts/Weapon/FoxTail.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class FoxTail : CursorTargetingMeleeWeapon
+{
+    private float[] _angles;
+    private Quaternion _firstStrikeRotation;
+
+    private void Awake()
+    {
+        int strikeMaxCount = Managers.Data.WeaponLevelTable[ItemID.TridentThrust][7].StrikeCount + 2;
+        _angles = new float[strikeMaxCount];
+    }
+
+    public override void LevelUp()
+    {
+        base.LevelUp();
+        Utils.SetAnglesFromCircle(_angles, weaponData.StrikeCount);
+    }
+
+    protected override void SetupStrikeOnPerform(WeaponStrike strike, int strikeIndex)
+    {
+        if (strikeIndex == 0)
+        {
+            _firstStrikeRotation = strike.transform.rotation;
+        }
+        strike.transform.rotation = _firstStrikeRotation * Quaternion.AngleAxis(_angles[strikeIndex], Vector3.back);
+
+        Managers.Sound.Play(SoundID.FoxTail);
+    }
+}
