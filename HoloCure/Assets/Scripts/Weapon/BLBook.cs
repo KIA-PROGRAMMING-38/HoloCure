ce28824fe737e223d4d5cc0fcd06caf776daf700
Yoo-JaeKwang <120005225+Yoo-JaeKwang@users.noreply.github.com>
2023-08-07 14:02:12 +0900

    [Refactor] #176 공전 무기 모듈화 (#177)

--- HoloCure/Assets/Scripts/Weapon/BLBook.cs
+++ HoloCure/Assets/Scripts/Weapon/BLBook.cs
@@ -1,56 +1,7 @@
-using UnityEngine;
-
-public class BLBook : Weapon
+public class BLBook : RevolvingWeapon
 {
-    private Vector2[] _projectileOffsets;
-    public override void LevelUp()
+    protected override void SetupStrikeOnOperate(WeaponStrike strike)
     {
-        base.LevelUp();
-
-        SetProjectileOffset();
-    }
-
-    private void SetProjectileOffset()
-    {
-        _projectileOffsets = new Vector2[weaponData.ProjectileCount];
-
-        int angleStep = 360 / weaponData.ProjectileCount;
-
-        for (int i = 0; i < weaponData.ProjectileCount; ++i)
-        {
-            float angle = i * angleStep * Mathf.Deg2Rad;
-            _projectileOffsets[i] = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * weaponData.Radius;
-        }
-    }
-
-    protected override void ShootProjectile(int projectileIndex)
-    {
-        Projectile projectile = Managers.Spawn.Projectile.Get();
-        Vector2 projectileInitPosition = weapon2DPosition + _projectileOffsets[projectileIndex];
-
-        projectile.Init(projectileInitPosition, weaponData, weaponCollider,
-            ProjectileOperate, offset: _projectileOffsets[projectileIndex]);
-    }
-
-    private void ProjectileOperate(Projectile projectile)
-    {
-        float angle = weaponData.ProjectileSpeed * Time.deltaTime;
-        projectile.Offset = GetNextOffset(projectile.Offset, angle);
-
-        projectile.transform.position = weapon2DPosition + projectile.Offset;
-        projectile.transform.rotation = default;
-
-        static Vector2 GetNextOffset(Vector2 offset, float degrees)
-        {
-            float angle = -degrees * Mathf.Deg2Rad;
-
-            float sin = Mathf.Sin(angle);
-            float cos = Mathf.Cos(angle);
-
-            float newX = (cos * offset.x) - (sin * offset.y);
-            float newY = (sin * offset.x) + (cos * offset.y);
-
-            return new Vector2(newX, newY);
-        }
+        strike.transform.rotation = default;
     }
 }
